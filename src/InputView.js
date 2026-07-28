1a22a5aa10184cbec310f3a83cc77bfb9063d34b
yenadaa <rlaekdpsk33@naver.com>
2026-07-28 22:56:30 +0900

    feat: 입력 기능 구현

---
+++ src/InputView.js
@@ -0,0 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class InputClass {
+    
+    async input() {
+        const carName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+        const attemptCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+        return {carName, attemptCount};
+    }
+}
+
+export default InputClass
