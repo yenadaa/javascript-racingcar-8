67916ef32ff3fc153ecab361bdbfd7b25abe8f91
yenadaa <rlaekdpsk33@naver.com>
2026-07-30 17:38:54 +0900

    feat: distace에 '-'를 추가는 방식으로 변경_주석추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -3,26 +3,38 @@ export class RacingClass {
         this.attemptCount = attemptCount;
     }
     randomNumber() {
+        // 0~9 사이 무작위 값을 생성하여 반환
         const number = MissionUtils.Random.pickNumberInRange(0, 9);
         return number;
     }
     goStop(number) {
-        return number >= 4;
+        // 무작위 값이 4 이상이면 true 반환
+        return number >= 4; 
     }
     moveCar(nameDistance) {
-        nameDistance.forEach(element => {
+        // 배열 안에 있는 각 자동차 객체에 순서대로 접근 => n대의 자동차를 경주시켜야 하기 때문
+        nameDistance.forEach(car => {
+            // 무작위 값이 4이상이면 전진!
             if(this.goStop(this.randomNumber())){
-                element.distance += 1;
+                // 전진 시 객체 안에 있는 distance에 '-' 추가
+                car.distance += '-';
             }
         });
     }
     race(nameDistance) {
-        let prograss = [];
+        // 각 차수별 경기상황을 저장할 배열 생성
+        let progress = [];
+
+        // 시도횟수만큼 moveCar를 반복할 for문
         for(let i = 0; i < this.attemptCount; i++) {
+            // moveCar - 경주 실행
             this.moveCar(nameDistance);
-            prograss.push([...nameDistance]);
+            // 경주가 끝난 후 그 회차의 경기상황이 저장돼있는 nameDistance배열을 progress라는 배열에 넣는다.
+            progress.push([...nameDistance]);
         }
-        return prograss;
+        
+        // 각 차수별로 저장돼있는 실행결과 모음 배열 반환
+        return progress;
     }
 
 }
