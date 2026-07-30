770a9d6e7120a19e02df4d202dbbe82401488a15
yenadaa <rlaekdpsk33@naver.com>
2026-07-30 17:13:11 +0900

    feat: distance의 기본값을 빈 문자열로 변경_주석추가

--- src/Parsing.js
+++ src/Parsing.js
@@ -1,6 +1,7 @@
 export class ParsingClass {
     parse(carName) {
-        return carName.split(',').map(name => ({name, distance: ''})); // 문자열을 쉼표를 기준으로 나누어 배열로 만들고 배열 안 요소를 name으로 잡고 거리를 적을 공간까지 매핑
+        return carName.split(',').map(name => ({name, distance: ''})); 
+        // 문자열을 쉼표를 기준으로 나누어 배열로 만들고 배열 안 요소를 name으로 잡고 거리를 적을 공간까지 매핑
     }
 }
 
