c5d2d521660d8b765238714e3d7a5e26bab430c4
yenadaa <rlaekdpsk33@naver.com>
2026-07-30 23:44:19 +0900

    feat: number -> count로 변수명 변경

--- src/Validation.js
+++ src/Validation.js
@@ -26,18 +26,18 @@ export class ValidationError {
             throw new Error('[ERROR] 시도횟수를 적어주세요!');
         }
 
-        const number = Number(attemptCount);
+        const count = Number(attemptCount);
         
-        if(Number.isNaN(number)){
+        if(Number.isNaN(count)){
             throw new Error('[ERROR] 시도횟수에 숫자를 적어주세요!');
         }
         // 이 코드가 맨 위로 가버리면 만약 시도횟수가 'abc'일때라면 숫자형으로 반환할 시 NaN이 되기 때문에
         // 0 이하인걸 확인하는 코드에서 false가 나옴 순서 중요!
         // 위 코드 NaN인 걸 확인하는 코드에서 걸러야 함!
-        if(number <= 0){
+        if(count <= 0){
             throw new Error('[ERROR] 시도횟수가 0 이하입니다!');
         }
-        if(number % 1 != 0){
+        if(count % 1 != 0){
           throw new Error('[ERROR] 시도횟수가 정수가 아닙니다!')  
         }
     }
